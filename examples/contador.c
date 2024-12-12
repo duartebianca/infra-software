f442c48d5addded41873186a2bdc550ab863ffc6
duartebianca <biancaduarte1914@gmail.com>
2024-12-12 15:34:25 -0300

    feat: add contador.c file with multi-threading implementation

---
+++ examples/contador.c
@@ -0,0 +1,24 @@
+#include <pthread.h>
+#include <stdio.h>
+long contador = 0; 
+void *inc(void *threadid){
+  int i = 0; 
+  for(; i < 9000000; i++) { 
+    contador++; 
+   }
+}
+void *dec(void *threadid){   
+  int i = 0;
+  for(; i < 9000000; i++) { 
+    contador--; 
+   }
+}
+int main (int argc, char *argv[]){   
+  pthread_t thread1, thread2;   
+  pthread_create(&thread1, NULL, inc, NULL); 
+  pthread_create(&thread2, NULL, dec, NULL); 
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL); 
+  printf("Valor final do contador: %ld\n", contador);
+  pthread_exit(NULL);
+}
